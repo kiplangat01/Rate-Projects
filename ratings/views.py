571de0f607eee8b14b542e8807f5616724e00925
kiplangat01 <97243798+kiplangat01@users.noreply.github.com>
2022-06-16 22:59:10 +0300

    ft:made average for ratings

--- ratings/views.py
+++ ratings/views.py
@@ -39,24 +39,48 @@ def add_Project(request):
         form = projectForm()
     return render(request,'ratings/upload.html',{'form':form})    
 
-def projects(request,id):
-    proj = Project.objects.get(id = id)
-    return render(request,'/readmore.html',{"projects":proj})
-
 
 @login_required(login_url='log_user')   
-def rate(request,id):
-
-    project = Project.objects.get(id = id)
-    user = request.user
+def project(request, post):
+    post = Project.objects.get(title=post)
+    ratings = Rating.objects.filter(user=request.user, post=post).first()
+    rating_status = None
+    if ratings is None:
+        rating_status = False
+    else:
+        rating_status = True
     if request.method == 'POST':
         form = RateForm(request.POST)
         if form.is_valid():
             rate = form.save(commit=False)
-            rate.user = user
-            rate.projects = project
+            rate.user = request.user
+            rate.post = post
             rate.save()
-            return redirect('index')
+            post_ratings = Rating.objects.filter(post=post)
+
+            design_ratings = [d.design for d in post_ratings]
+            design_average = sum(design_ratings) / len(design_ratings)
+
+            usability_ratings = [us.usability for us in post_ratings]
+            usability_average = sum(usability_ratings) / len(usability_ratings)
+
+            content_ratings = [content.content for content in post_ratings]
+            content_average = sum(content_ratings) / len(content_ratings)
+
+            score = (design_average + usability_average + content_average) / 3
+            print(score)
+            rate.design_average = round(design_average, 2)
+            rate.usability_average = round(usability_average, 2)
+            rate.content_average = round(content_average, 2)
+            rate.score = round(score, 2)
+            rate.save()
+            return HttpResponseRedirect(request.path_info)
     else:
         form = RateForm()
+    params = {
+        'post': post,
+        'rating_form': form,
+        'rating_status': rating_status
+
+    }
     return render(request,"ratings/projectrating.html",{"form":form,"project":project})     
