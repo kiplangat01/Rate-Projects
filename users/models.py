205bbe4526287ca1a351483d06eab189bbe34f74
kiplangat01 <97243798+kiplangat01@users.noreply.github.com>
2022-06-18 15:10:44 +0300

    ft:polished entire app

--- users/models.py
+++ users/models.py
@@ -2,23 +2,31 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from PIL import Image
-
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    username = models.CharField(max_length=50, null=True, blank=True)
-    location = models.CharField(max_length=50, null=True, blank=True)
-    picture = CloudinaryField('image', default='default.jpg', null=True)
-    # picture = models.ImageField( upload_to='profile_pics')
+  user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+  bio = models.TextField(max_length=200, blank=True,)
+  picture = CloudinaryField('image', blank=True,)
+  firstname = models.CharField(blank=True, max_length=120)
+  lastname = models.CharField(blank=True, max_length=120)
 
+  def save(self, *args, **kwargs):
+    super().save(*args, **kwargs)
+    SIZE = 250, 250
 
-    def __str__(self):
-        return f'{self.user.username} Profile'
+  def __str__(self):
+    return self.user.username
 
-    
+  @receiver(post_save, sender=User)
+  def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+      Profile.objects.create(user=instance)
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+  @receiver(post_save, sender=User)
+  def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 
         
