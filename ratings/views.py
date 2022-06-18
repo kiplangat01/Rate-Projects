3aac288f7251fa09b2c6d49993c9f738dcd3ddb6
kiplangat01 <97243798+kiplangat01@users.noreply.github.com>
2022-06-18 15:01:46 +0300

    ft:polished up the entire project

--- ratings/views.py
+++ ratings/views.py
@@ -3,16 +3,16 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .models import *
 from users.models import Profile
-from .forms import projectForm,RateForm
+from .forms import NewPostForm, RateForm
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 import random
 
-
+@login_required
 def index(request):
     user = request.user
     if request.method == "POST":
-        form = projectForm(request.POST, request.FILES)
+        form = NewPostForm(request.POST, request.FILES)
         if form.is_valid():
             title = form.cleaned_data.get('title')
             image = request.FILES.get('image')
@@ -25,7 +25,7 @@ def index(request):
             post.save()
         return HttpResponseRedirect('/')
     else:
-        form = projectForm()
+        form = NewPostForm()
 
     try:
         posts = Project.objects.all().order_by('-date_posted')
@@ -34,7 +34,7 @@ def index(request):
         print(random_post.image)
     except Project.DoesNotExist:
         posts = None
-    return render(request, 'index.html', {'posts': posts, 'form': form, 'random_post': random_post})
+    return render(request, 'ratings/index.html', {'posts': posts, 'form': form, 'random_post': random_post})
 
     
 def searchproject(request):
@@ -45,16 +45,16 @@ def searchproject(request):
             'results': results
             
         }
-        return render(request, 'search.html', params)
+        return render(request, 'ratings/results.html', params)
     else:
         message = "You haven't searched for any profile"
-    return render(request, 'results.html')
+    return render(request, 'reviews/results.html')
 
 
 @login_required(login_url='login')
-def project(request, post):
-    post = Project.objects.get(title=post)
-    ratings = Rating.objects.filter(user=request.user, post=post).first()
+def project(request, post_id):
+    post = Project.objects.get(id=post_id)
+    ratings = Rating.objects.filter(user=request.user, post_id=post_id).first()
     rating_status = None
     if ratings is None:
         rating_status = False
@@ -67,7 +67,7 @@ def project(request, post):
             rate.user = request.user
             rate.post = post
             rate.save()
-            post_ratings = Rating.objects.filter(post=post)
+            post_ratings = Rating.objects.filter(post_id=post_id)
 
             design_ratings = [d.design for d in post_ratings]
             design_average = sum(design_ratings) / len(design_ratings)
@@ -94,4 +94,4 @@ def project(request, post):
         'rating_status': rating_status
 
     }
-    return render(request,"ratings/projectrating.html",{"form":form,"project":project})     
+    return render(request, 'ratings/ratings_project.html', params)     
